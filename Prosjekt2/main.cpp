670213c605198aa1548d1b030eb1b32d16f9ea08
VidarLuOl <vidarlol@student.matnat.uio.no>
2019-09-20 12:04:05 +0200

    Prøvde å finne egenverdier til matrisen

--- Prosjekt2/main.cpp
+++ Prosjekt2/main.cpp
@@ -13,20 +13,26 @@ int main() {
 
     Mat<double> A(n, n, fill::zeros);
 
-    for(unsigned int i=0; i<n; i++){
+    for(unsigned int i=0; i<n-1; i++){
         A(i,i) = d;
+        A(i+1,i) = a;
+        A(i,i+1) = a;
     }
 
-    for(unsigned int i=0; i<n-3; i++){
-        A(i+3,i) = a;
-        A(i,i+3) = a;
-    }
+    A(n-1, n-1) = d;
+
 
     Mat<double> P, L, U;
 
     lu(P, L, U, A);
 
-    cout << A << endl << P << endl << L << endl << U << endl;
+    cout << L << endl;
 
+    vec eigval;
+    mat eigvec;
+
+    eig_sym(eigval, eigvec, A);
+
+    cout << eigval << endl;
     return 0;
 }
