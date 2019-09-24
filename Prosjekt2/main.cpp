07e22565aececd26df0e9cac86dc33dd017c30c7
Elin Finstad <elin.finstad@hotmail.com>
2019-09-24 13:36:58 +0200

    Added timing

--- Prosjekt2/main.cpp
+++ Prosjekt2/main.cpp
@@ -2,46 +2,48 @@
 #include "jacobi.h"  //Gathers functions from jacobi.cpp
 #include <armadillo> //Matrix maker
 #include <math.h>    //Sin, cos
+#include <chrono>
 
 using namespace std;
 using namespace arma;
 
 
 int main() {
-    unsigned int n = 3;
+    unsigned int n = 10;
     double h = 1.0/n;
-    double d = 2/h*h;
-    double a = -1/h*h;
+    double d = 2/(h*h);
+    double a = -1/(h*h);
 
     Mat<double> A(n, n, fill::zeros);
+    Mat<double> R(n, n, fill::zeros);
 
+    // initializing matrix A
     for(unsigned int i=0; i<n-1; i++){
         A(i,i) = d;
         A(i+1,i) = a;
         A(i,i+1) = a;
     }
-
     A(n-1, n-1) = d;
-
-
-    Mat<double> P, L, U;
-
-    lu(P, L, U, A);
-
-    cout << L << endl;
 
     vec eigval;
     mat eigvec;
 
-    Mat<double> R(n, n, fill::zeros);
+    // start timing
+    auto start = chrono::high_resolution_clock::now();
 
-    //Skal sammenligne eigval med verdiene fra rotasjon funksjonen i jacobi.cpp
+    // Calculating eigenpairs with armadillo
     eig_sym(eigval, eigvec, A);
 
-    //Må finne ut en måte å kalle på funksjonen
+    // end timing
+    auto finish = chrono::high_resolution_clock::now();
+
+    // print time
+    chrono::duration<double> elapsed = (finish - start);
+    cout << "Armadillo eig_sym: " << elapsed.count() << " s\n";
+
+    // calculating eigenvectors with Jacobi's method
     jacobi_method(A, R, n);
 
-
-    cout << eigval << " " << eigvec << endl;
+//    cout << R << "\n " << eigvec << endl;
     return 0;
 }
