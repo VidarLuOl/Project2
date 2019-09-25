8301bd9d481d1baa8b592050e2fd04ca01eed0e6
Elin Finstad <elin.finstad@hotmail.com>
2019-09-25 15:44:48 +0200

    Adding test function for orthogonality. Approx does not work for zero

--- Prosjekt2/test-jacobi.cpp
+++ Prosjekt2/test-jacobi.cpp
@@ -44,3 +44,57 @@ TEST_CASE("Test eigenvalues"){
     }
 }
 
+TEST_CASE("Test preserved orthogonality"){
+    int n = 5;
+    // Set up exact eigenvalues
+    vec exact_eigval(n);
+
+    double pi = acos(-1.0);         // ?????????????????????
+    double h = 1.0/n;
+    double d = 2.0/(h*h);
+    double a = -1.0/(h*h);
+
+    for (int i = 0; i < n; i++) {
+       exact_eigval(i) = d + 2*a*cos((i+1)*pi/(n + 1));
+    }
+
+    // Get numerical eigenvalues
+    vec num_eigvec(n);
+
+    Mat<double> A(n, n, fill::zeros);
+    Mat<double> R(n, n, fill::zeros);
+    Mat<double> U(n, n, fill::zeros);
+
+
+    // initializing matrix A
+    for(int i=0; i<n-1; i++){
+        A(i,i) = d;
+        A(i+1,i) = a;
+        A(i,i+1) = a;
+        U(i,i) = 1;
+    }
+    A(n-1, n-1) = d;
+    U(n-1, n-1) = 1;
+
+    U(n/2-1, n-1) = -sin(pi/4);
+    U(n-1, n/2-1) = sin(pi/4);
+    U(n-1, n-1) = cos(pi/4);
+    U(n/2-1, n/2-1) = cos(pi/4);
+
+    jacobi_method(A, R, n);
+
+    cout << dot(U*R.col(0), U*R.col(0)) << endl;
+
+    for (int i=0; i < n; i++) {
+        for (int j=0; j < n; j++) {
+            if (i == j) {
+                REQUIRE(dot(U*R.col(i), U*R.col(i))==Approx(1.0).epsilon(0.000000000001));
+            }
+            else {
+//                cout << dot(U*R.col(j), U*R.col(i)) << endl;
+                REQUIRE((dot(U*R.col(j), U*R.col(i)))==Approx(0.0).epsilon(0.1));
+            }
+        }
+    }
+
+}
