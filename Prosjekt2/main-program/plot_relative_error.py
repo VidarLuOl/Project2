60c1971a4041221837ebf8dc8618d8229d634615
Elin Finstad <elin.finstad@hotmail.com>
2019-10-01 14:00:20 +0200

    Plotting the relative error

--- Prosjekt2/main-program/plot_relative_error.py
+++ Prosjekt2/main-program/plot_relative_error.py
@@ -1,4 +1,48 @@
-# This Python file uses the following encoding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
 
-# if__name__ == "__main__":
-#     pass
+
+def read_file(filename):
+    infile = open(filename, "r")
+
+    N = []
+    rho_max = []
+    average = []
+
+    for line in infile:
+        n, r, avr = line.split()
+        N.append(int(n))
+        rho_max.append(float(r))
+        average.append(float(avr))
+
+    return np.array(N), np.array(rho_max), np.array(average)
+
+
+for i in [10, 25, 50, 100, 200]:
+    filename = "error-n-" + str(i)
+    N, rho_max, average = read_file(filename)
+
+    plt.plot(rho_max[0:-1], average[0:-1], label = "n = " + str(i))
+
+plt.legend(loc="best")
+plt.xlabel("rho_max", fontsize=15)
+plt.ylabel("Average Relative Error", fontsize=15)
+plt.title("Average Realtive Error", fontsize=25)
+plt.grid()
+plt.show()
+
+
+for i in [10, 25, 50, 100, 200]:
+    filename = "error-small-n-" + str(i)
+    N, rho_max, average = read_file(filename)
+
+#    plt.plot(rho_max, average, 'o')
+    plt.plot(rho_max, average, label = "n = " + str(i))
+
+plt.legend()
+plt.xlabel("rho_max", fontsize=15)
+plt.ylabel("Average Relative Error", fontsize=15)
+plt.title("Average Realtive Error", fontsize=25)
+plt.grid()
+plt.show()
+
